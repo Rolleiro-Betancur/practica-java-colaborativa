d2a3acb76047a90e779c1ad2d7360c913de86542
rogerbetan201@gmail.com <Rolleiro-betancur>
2025-09-15 09:10:59 -0500

    Creando la clase Producto con sus atributos y constructor

---
+++ src/Producto.java
@@ -0,0 +1,17 @@
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private float precio;
+    private String tipo;
+
+    public Producto(int id, String nombre, float precio, String tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.tipo = tipo;
+    }
+
+
+
+}
