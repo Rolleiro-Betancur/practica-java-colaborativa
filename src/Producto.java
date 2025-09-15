a6bb0487115665d18cb07a32212fc3ab4475796b
brandonbmor <brandon107766@gmail.com>
2025-09-15 09:42:02 -0500

    REALIZE METODO ACTUALIZAR STOCK

--- src/Producto.java
+++ src/Producto.java
@@ -4,14 +4,20 @@ public class Producto {
     private String nombre;
     private float precio;
     private String tipo;
+    private  String stock;
 
-    public Producto(int id, String nombre, float precio, String tipo) {
+    public Producto(int id, String nombre, float precio, String tipo, String stock) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.tipo = tipo;
+        this.stock=stock;
     }
-
-
+    public void actualizarstock (String stock){
+        this.stock=stock;
+    }
+    public int getId() {
+        return id;
+    }
 
 }
